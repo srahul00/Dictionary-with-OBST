d61c71a75611830f3d7fe0777c481750c53b3aa7
SRahul <rrahul849@gmail.com>
2020-10-27 00:49:38 +0530

    Added utility fn to select a percentage of words

--- OBST.cpp
+++ OBST.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 using namespace std;
 
 class OBST
@@ -6,8 +8,50 @@ class OBST
 
 };
 
+//Utility function to get a percentage of words (uniformly skipping rows)
+//Creates a file with format: word " " probability
+void getWords(string inFilename, string outFilename, float percentage = 15.0/100)
+{
+    string word;
+    int freq;
+    vector<pair<string, int>> words;
+    long long totalcount = 0;
+    int i = 0;
+    int counter = 1.0/percentage;
+
+    //Reading from the in file
+    //Storing 1 line every 'counter' no.of lines
+    fstream f;
+    f.open(inFilename, ios::in);
+
+    while(f>>word)
+    {
+        f>>freq;
+        i++;
+        i %= counter;
+
+        if(i % counter)
+            continue;
+
+        words.push_back({word, freq});
+        totalcount += freq;
+    }
+    f.close();
+
+    //Writing into the out file
+    fstream g;
+    g.open(outFilename, ios::out);
+    for(auto p: words)
+    {
+        g<<p.first<<" "<< (long double) 1.0 * p.second / totalcount <<"\n";
+    }
+    g.close();
+
+}
+
 int main()
 {
+    //getWords("count_big.txt", "words.txt");
     OBST obj = OBST();
     return 0;
 }
