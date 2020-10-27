7f01de9c8c6b6c226c36651e85f6da75cc9e7559
SRahul <rrahul849@gmail.com>
2020-10-27 10:40:08 +0530

    Utility fn for selecting words

--- OBST.cpp
+++ OBST.cpp
@@ -10,12 +10,12 @@ class OBST
 
 //Utility function to get a percentage of words (uniformly skipping rows)
 //Creates a file with format: word " " probability
-void getWords(string inFilename, string outFilename, float percentage = 15.0/100)
+void getWords(string inFilename, string outFilename, float percentage = 10.0/100)
 {
     string word;
     int freq;
-    vector<pair<string, int>> words;
-    long long totalcount = 0;
+    vector<pair<string, int>> words; //Storing the file data in a vector
+    long long totalcount = 0; //Total count to get the probability value by dividing
     int i = 0;
     int counter = 1.0/percentage;
 
@@ -51,7 +51,7 @@ void getWords(string inFilename, string outFilename, float percentage = 15.0/100
 
 int main()
 {
-    //getWords("count_big.txt", "words.txt");
+    //getWords("count_big.txt", "words.txt", 3.0/100);
     OBST obj = OBST();
     return 0;
 }
